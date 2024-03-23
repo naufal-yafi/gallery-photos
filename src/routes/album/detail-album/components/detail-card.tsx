c0d5162a19ce174854d7eaa6ed93d4297fb29534
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2024-03-23 12:54:37 +0700

    popup action update and delete

--- src/routes/album/detail-album/components/detail-card.tsx
+++ src/routes/album/detail-album/components/detail-card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PhotoType from "../../../../types/photo.type";
+import MoreVerticalDot from "../../partials/components/more-vertical-dot/more-vertical-dot.tsx";
 import formatDate from "../lib/format-date.ts";
 
 export default function DetailCard({ photo }: Readonly<{ photo: PhotoType }>) {
@@ -14,6 +15,7 @@ export default function DetailCard({ photo }: Readonly<{ photo: PhotoType }>) {
       </figure>
 
       <figcaption className="mt-4 md:mt-0">
+        <MoreVerticalDot id={photo.id} date={photo.createdAt} />
         <h1>{photo?.captions}</h1>
         <p className="mt-2 text-xs opacity-80">
           {photo?.createdAt !== photo?.updatedAt
--- src/routes/album/partials/components/card.tsx
+++ src/routes/album/partials/components/card.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PhotoType from "../../../../types/photo.type.ts";
+import MoreVerticalDot from "./more-vertical-dot/more-vertical-dot.tsx";
 
 const Card = ({ photo }: { photo: PhotoType }) => {
   return (
@@ -13,13 +14,15 @@ const Card = ({ photo }: { photo: PhotoType }) => {
         />
       </Link>
 
-      <figcaption className="py-2">
+      <figcaption className="flex items-start justify-between py-2">
         <h3
           className="mb-4 text-base font-medium truncate"
           data-testid="photo-caption"
         >
           {photo.captions}
         </h3>
+
+        <MoreVerticalDot id={photo.id} date={photo.createdAt} />
       </figcaption>
     </figure>
   );
